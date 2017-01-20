0bde4057f80acb78624f460236a96ed8dbd5d156
Sergey Khoroshavin <skhoroshavin@gmail.com>
2017-01-21 01:41:26 +0300

    Added generator interface

--- src/test_gen.c
+++ src/test_gen.c
@@ -1,3 +1,8 @@
 
 #include "test_gen.h"
 
+void qcc_test_gen_example(struct qcc_test_gen *self,
+                          struct qcc_test_context *ctx, void *data)
+{
+    self->example(self, ctx, data);
+}
--- src/test_gen.h
+++ src/test_gen.h
@@ -7,6 +7,17 @@
 extern "C" {
 #endif // __cplusplus
 
+struct qcc_test_gen
+{
+    size_t size;
+    size_t align;
+    void (*example)(struct qcc_test_gen *self, struct qcc_test_context *ctx,
+                    void *data);
+};
+
+void qcc_test_gen_example(struct qcc_test_gen *self,
+                          struct qcc_test_context *ctx, void *data);
+
 #ifdef __cplusplus
 }
 #endif // __cplusplus
