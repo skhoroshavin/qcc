d86d9a20f69214be5925379b98b24a33851cc336
Sergey Khoroshavin <skhoroshavin@gmail.com>
2017-01-21 18:15:30 +0300

    Implemented dynamic array allocation from arena

--- src/arena.c
+++ src/arena.c
@@ -12,8 +12,10 @@ void qcc_arena_init(struct qcc_arena *arena, size_t max_size)
 {
     arena->buffer_start = qcc_malloc(max_size);
     arena->buffer_end = arena->buffer_start + max_size;
-    arena->available_memory = arena->buffer_start;
+    arena->free_memory = arena->buffer_start;
     arena->objects = (struct qcc_arena_object *)arena->buffer_end;
+    arena->array_start = 0;
+    arena->array_end = 0;
 }
 
 void qcc_arena_done(struct qcc_arena *arena)
@@ -22,17 +24,27 @@ void qcc_arena_done(struct qcc_arena *arena)
     qcc_free(arena->buffer_start);
 }
 
+void qcc_arena_reset(struct qcc_arena *arena)
+{
+    while ((uint8_t *)arena->objects < arena->buffer_end)
+    {
+        arena->objects->dtor(arena->objects->ptr);
+        ++arena->objects;
+    }
+    arena->free_memory = arena->buffer_start;
+}
+
 size_t qcc_arena_memory_available(const struct qcc_arena *arena)
 {
-    return (uint8_t *)arena->objects - arena->available_memory;
+    return (uint8_t *)arena->objects - arena->free_memory;
 }
 
 void *qcc_arena_alloc(struct qcc_arena *arena, size_t size)
 {
     if (qcc_arena_memory_available(arena) < size) return 0;
 
-    void *ptr = arena->available_memory;
-    arena->available_memory += size;
+    void *ptr = arena->free_memory;
+    arena->free_memory += size;
     return ptr;
 }
 
@@ -51,7 +63,7 @@ unsigned qcc_arena_add_object(struct qcc_arena *arena, void *ptr,
 const char *qcc_arena_vsprintf(struct qcc_arena *arena, const char *fmt,
                                va_list args)
 {
-    char *str = (char *)arena->available_memory;
+    char *str = (char *)arena->free_memory;
 
     va_list tmp;
     va_copy(tmp, args);
@@ -60,7 +72,7 @@ const char *qcc_arena_vsprintf(struct qcc_arena *arena, const char *fmt,
 
     if (len < qcc_arena_memory_available(arena))
     {
-        arena->available_memory += len + 1;
+        arena->free_memory += len + 1;
         return str;
     }
 
@@ -77,12 +89,23 @@ const char *qcc_arena_sprintf(struct qcc_arena *arena, const char *fmt, ...)
     return str;
 }
 
-void qcc_arena_reset(struct qcc_arena *arena)
+void qcc_arena_begin_array(struct qcc_arena *arena)
 {
-    while ((uint8_t *)arena->objects < arena->buffer_end)
-    {
-        arena->objects->dtor(arena->objects->ptr);
-        ++arena->objects;
-    }
-    arena->available_memory = arena->buffer_start;
+    arena->array_start = arena->free_memory;
+    arena->array_end = arena->array_start;
+}
+
+void qcc_arena_append_array(struct qcc_arena *arena, void *data, size_t size)
+{
+    memcpy(arena->array_end, data, size);
+    arena->array_end += size;
+}
+
+void *qcc_arena_end_array(struct qcc_arena *arena)
+{
+    void *res = arena->array_start;
+    arena->free_memory = arena->array_end;
+    arena->array_start = 0;
+    arena->array_end = 0;
+    return res;
 }
--- src/arena.h
+++ src/arena.h
@@ -7,7 +7,7 @@
 
 #ifdef __cplusplus
 extern "C" {
-#endif 
+#endif
 
 typedef void (*qcc_destroy_fn)(void *);
 struct qcc_arena_object
@@ -20,20 +20,28 @@ struct qcc_arena
 {
     uint8_t *buffer_start;
     uint8_t *buffer_end;
-    uint8_t *available_memory;
+    uint8_t *free_memory;
     struct qcc_arena_object *objects;
+    uint8_t *array_start;
+    uint8_t *array_end;
 };
 
 void qcc_arena_init(struct qcc_arena *arena, size_t max_size);
 void qcc_arena_done(struct qcc_arena *arena);
+void qcc_arena_reset(struct qcc_arena *arena);
 size_t qcc_arena_memory_available(const struct qcc_arena *arena);
+
 void *qcc_arena_alloc(struct qcc_arena *arena, size_t size);
 unsigned qcc_arena_add_object(struct qcc_arena *arena, void *ptr,
                               qcc_destroy_fn dtor);
+
 const char *qcc_arena_vsprintf(struct qcc_arena *arena, const char *fmt,
                                va_list args);
 const char *qcc_arena_sprintf(struct qcc_arena *arena, const char *fmt, ...);
-void qcc_arena_reset(struct qcc_arena *arena);
+
+void qcc_arena_begin_array(struct qcc_arena *arena);
+void qcc_arena_append_array(struct qcc_arena *arena, void *data, size_t size);
+void *qcc_arena_end_array(struct qcc_arena *arena);
 
 #define QCC_ARENA_POD(arena, type, name)                                       \
     struct type *name =                                                        \
@@ -46,4 +54,4 @@ void qcc_arena_reset(struct qcc_arena *arena);
 
 #ifdef __cplusplus
 }
-#endif 
+#endif
