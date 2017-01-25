5bd2157b4ddbbb140e6bd1262db67631bace7e33
Sergey Khoroshavin <skhoroshavin@gmail.com>
2017-01-26 02:18:28 +0300

    uint generation improvements

--- src/qcc_uint.c
+++ src/qcc_uint.c
@@ -6,8 +6,8 @@
 struct _generator_uint_in_range
 {
     struct qcc_generator base;
-    unsigned min;
-    unsigned max;
+    qcc_uint min;
+    qcc_uint max;
 };
 
 static size_t _uint_type_size(enum qcc_uint_type type)
@@ -26,7 +26,7 @@ static size_t _uint_type_size(enum qcc_uint_type type)
     return 0;
 }
 
-static unsigned _limit_uint(enum qcc_uint_type type, unsigned value)
+static qcc_uint _limit_uint(enum qcc_uint_type type, qcc_uint value)
 {
     switch (type)
     {
@@ -44,11 +44,12 @@ static unsigned _limit_uint(enum qcc_uint_type type, unsigned value)
 static void _generate_uint_in_range(struct qcc_generator *self,
                                     struct qcc_context *ctx, void *data)
 {
-    unsigned min = ((struct _generator_uint_in_range *)self)->min;
-    unsigned max = ((struct _generator_uint_in_range *)self)->max;
+    qcc_uint min = ((struct _generator_uint_in_range *)self)->min;
+    qcc_uint max = ((struct _generator_uint_in_range *)self)->max;
 
-    unsigned result = qcc_context_rand_value(ctx);
-    unsigned range = max - min + 1;
+    qcc_uint result;
+    qcc_context_rand(ctx, &result, sizeof(result));
+    qcc_uint range = max - min + 1;
     if (range) result = result % range + min;
 
     switch (self->type_size)
@@ -70,7 +71,7 @@ static void _generate_uint_in_range(struct qcc_generator *self,
 
 static struct qcc_generator *_gen_uint_in_range(struct qcc_context *ctx,
                                                 enum qcc_uint_type type,
-                                                unsigned min, unsigned max)
+                                                qcc_uint min, qcc_uint max)
 {
     QCC_ARENA_POD(ctx->arena, _generator_uint_in_range, res);
     res->base.type_size = _uint_type_size(type);
@@ -82,14 +83,14 @@ static struct qcc_generator *_gen_uint_in_range(struct qcc_context *ctx,
 
 struct qcc_generator *qcc_gen_uint_equal_to(struct qcc_context *ctx,
                                             enum qcc_uint_type type,
-                                            unsigned value)
+                                            qcc_uint value)
 {
     return _gen_uint_in_range(ctx, type, value, value);
 }
 
 struct qcc_generator *qcc_gen_uint_in_range(struct qcc_context *ctx,
                                             enum qcc_uint_type type,
-                                            unsigned min, unsigned max)
+                                            qcc_uint min, qcc_uint max)
 {
     min = _limit_uint(type, min);
     max = _limit_uint(type, max);
@@ -97,7 +98,7 @@ struct qcc_generator *qcc_gen_uint_in_range(struct qcc_context *ctx,
 
     if (max - min < 20) return _gen_uint_in_range(ctx, type, min, max);
 
-    unsigned avg = min / 2 + max / 2;
+    qcc_uint avg = min / 2 + max / 2;
     return qcc_gen_one_of(ctx, _gen_uint_in_range(ctx, type, min, min + 2),
                           _gen_uint_in_range(ctx, type, max - 2, max),
                           _gen_uint_in_range(ctx, type, avg - 1, avg + 1),
@@ -109,39 +110,45 @@ struct qcc_generator *qcc_gen_uint_in_range(struct qcc_context *ctx,
 
 struct qcc_generator *qcc_gen_uint_less_than(struct qcc_context *ctx,
                                              enum qcc_uint_type type,
-                                             unsigned value)
+                                             qcc_uint value)
 {
     return qcc_gen_uint_in_range(ctx, type, 0, value - 1);
 }
 
 struct qcc_generator *qcc_gen_uint_greater_than(struct qcc_context *ctx,
                                                 enum qcc_uint_type type,
-                                                unsigned value)
+                                                qcc_uint value)
 {
-    return qcc_gen_uint_in_range(ctx, type, value + 1, UINT32_MAX);
+    return qcc_gen_uint_in_range(ctx, type, value + 1, QCC_UINT_MAX);
 }
 
 struct qcc_generator *qcc_gen_uint_not_less_than(struct qcc_context *ctx,
                                                  enum qcc_uint_type type,
-                                                 unsigned value)
+                                                 qcc_uint value)
 {
-    return qcc_gen_uint_in_range(ctx, type, value, UINT32_MAX);
+    return qcc_gen_uint_in_range(ctx, type, value, QCC_UINT_MAX);
 }
 
 struct qcc_generator *qcc_gen_uint_not_greater_than(struct qcc_context *ctx,
                                                     enum qcc_uint_type type,
-                                                    unsigned value)
+                                                    qcc_uint value)
 {
     return qcc_gen_uint_in_range(ctx, type, 0, value);
 }
 
 struct qcc_generator *qcc_gen_uint_not_equal_to(struct qcc_context *ctx,
                                                 enum qcc_uint_type type,
-                                                unsigned value)
+                                                qcc_uint value)
 {
     if (value == 0) return qcc_gen_uint_greater_than(ctx, type, 0);
-    if (value == UINT32_MAX)
+    if ((type == QCC_UINT8) && (value == UINT8_MAX))
+        return qcc_gen_uint_less_than(ctx, type, UINT8_MAX);
+    if ((type == QCC_UINT16) && (value == UINT16_MAX))
+        return qcc_gen_uint_less_than(ctx, type, UINT16_MAX);
+    if ((type == QCC_UINT32) && (value == UINT32_MAX))
         return qcc_gen_uint_less_than(ctx, type, UINT32_MAX);
+    if ((type == QCC_UINT64) && (value == UINT64_MAX))
+        return qcc_gen_uint_less_than(ctx, type, UINT64_MAX);
     return qcc_gen_one_of(ctx, qcc_gen_uint_less_than(ctx, type, value),
                           qcc_gen_uint_greater_than(ctx, type, value),
                           (void *)0);
@@ -150,12 +157,22 @@ struct qcc_generator *qcc_gen_uint_not_equal_to(struct qcc_context *ctx,
 struct qcc_generator *qcc_gen_uint_any(struct qcc_context *ctx,
                                        enum qcc_uint_type type)
 {
-    return qcc_gen_uint_in_range(ctx, type, 0, UINT32_MAX);
+    return qcc_gen_uint_in_range(ctx, type, 0, QCC_UINT_MAX);
 }
 
 unsigned qcc_generate_uint(struct qcc_generator *self, struct qcc_context *ctx)
 {
     unsigned result;
+    assert(self->type_size == sizeof(result));
+    qcc_generate(self, ctx, &result);
+    return result;
+}
+
+uint64_t qcc_generate_uint64(struct qcc_generator *self,
+                             struct qcc_context *ctx)
+{
+    uint64_t result;
+    assert(self->type_size == sizeof(result));
     qcc_generate(self, ctx, &result);
     return result;
 }
--- src/qcc_uint.h
+++ src/qcc_uint.h
@@ -5,6 +5,9 @@
 
 QCC_BEGIN_HEADER
 
+typedef uint64_t qcc_uint;
+#define QCC_UINT_MAX UINT64_MAX
+
 enum qcc_uint_type
 {
     QCC_UINT8,
@@ -15,36 +18,46 @@ enum qcc_uint_type
 
 struct qcc_generator *qcc_gen_uint_equal_to(struct qcc_context *ctx,
                                             enum qcc_uint_type type,
-                                            unsigned value);
+                                            qcc_uint value);
 struct qcc_generator *qcc_gen_uint_in_range(struct qcc_context *ctx,
                                             enum qcc_uint_type type,
-                                            unsigned min, unsigned max);
+                                            qcc_uint min, qcc_uint max);
 struct qcc_generator *qcc_gen_uint_less_than(struct qcc_context *ctx,
                                              enum qcc_uint_type type,
-                                             unsigned value);
+                                             qcc_uint value);
 struct qcc_generator *qcc_gen_uint_greater_than(struct qcc_context *ctx,
                                                 enum qcc_uint_type type,
-                                                unsigned value);
+                                                qcc_uint value);
 struct qcc_generator *qcc_gen_uint_not_less_than(struct qcc_context *ctx,
                                                  enum qcc_uint_type type,
-                                                 unsigned value);
+                                                 qcc_uint value);
 struct qcc_generator *qcc_gen_uint_not_greater_than(struct qcc_context *ctx,
                                                     enum qcc_uint_type type,
-                                                    unsigned value);
+                                                    qcc_uint value);
 struct qcc_generator *qcc_gen_uint_not_equal_to(struct qcc_context *ctx,
                                                 enum qcc_uint_type type,
-                                                unsigned value);
+                                                qcc_uint value);
 struct qcc_generator *qcc_gen_uint_any(struct qcc_context *ctx,
                                        enum qcc_uint_type type);
 
 unsigned qcc_generate_uint(struct qcc_generator *self, struct qcc_context *ctx);
+uint64_t qcc_generate_uint64(struct qcc_generator *self,
+                             struct qcc_context *ctx);
 
 #define qcc_rand_uint(ctx, cond, ...)                                          \
     qcc_generate_uint(qcc_gen_uint_##cond(ctx, QCC_UINT32, ##__VA_ARGS__), ctx)
 
+#define qcc_rand_uint64(ctx, cond, ...)                                        \
+    qcc_generate_uint64(qcc_gen_uint_##cond(ctx, QCC_UINT64, ##__VA_ARGS__),   \
+                        ctx)
+
 #define GIVEN_UINT(name, cond, ...)                                            \
     unsigned name = qcc_rand_uint(_ctx, cond, ##__VA_ARGS__);                  \
     qcc_context_register_param(_ctx, "%s: %u", #name, name);
+
+#define GIVEN_UINT64(name, cond, ...)                                          \
+    unsigned name = qcc_rand_uint64(_ctx, cond, ##__VA_ARGS__);                \
+    qcc_context_register_param(_ctx, "%s: %ul", #name, name);
 
 #define GIVEN_UINT_ARRAY(name, asize, cond, ...)                               \
     struct                                                                     \
@@ -58,7 +71,33 @@ unsigned qcc_generate_uint(struct qcc_generator *self, struct qcc_context *ctx);
                  _ctx, &name);                                                 \
     qcc_context_register_param(_ctx, "%s_size: %zu", #name, name.size);
 
+#define GIVEN_UINT64_ARRAY(name, asize, cond, ...)                             \
+    struct                                                                     \
+    {                                                                          \
+        uint64_t *data;                                                        \
+        size_t size;                                                           \
+    } name;                                                                    \
+    qcc_generate(qcc_gen_array_of(                                             \
+                     _ctx, qcc_array_##asize,                                  \
+                     qcc_gen_uint_##cond(_ctx, QCC_UINT64, ##__VA_ARGS__)),    \
+                 _ctx, &name);                                                 \
+    qcc_context_register_param(_ctx, "%s_size: %zu", #name, name.size);
+
 #define ASSERT_UINT(got, cond, exp)                                            \
+    do                                                                         \
+    {                                                                          \
+        if (!((got)cond(exp)))                                                 \
+        {                                                                      \
+            qcc_context_fail(                                                  \
+                _ctx, "Assertion \"%s %s %s\" (%u %s %u) failed in "           \
+                      "%s (%s, line %d)\n",                                    \
+                #got, #cond, #exp, (unsigned)(got), #cond, (unsigned)(exp),    \
+                __FUNCTION__, __FILE__, __LINE__);                             \
+            return;                                                            \
+        }                                                                      \
+    } while (0)
+
+#define ASSERT_UINT64(got, cond, exp)                                          \
     do                                                                         \
     {                                                                          \
         if (!((got)cond(exp)))                                                 \
